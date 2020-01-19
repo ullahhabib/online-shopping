6bedd0ffb8b32bebdfbca92a0519a1756d9c7dea
Syed Habib <ullahhabib9@gmail.com>
2020-01-19 14:10:48 -0600

    My project setup completed successfully

---
+++ shoppingbackend/src/main/java/net/hu/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.hu.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
